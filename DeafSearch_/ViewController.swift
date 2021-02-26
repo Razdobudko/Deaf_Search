e424dab0daa0a378cd7f339b94766fc9743e49cf
Razdabudzka <razdabudzka@icloud.com>
2021-02-27 01:08:46 +0300

    Initial Commit

---
+++ DeafSearch_/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeafSearch_
+//
+//  Created by Veranika Razdabudzka on 27.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
